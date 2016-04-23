cc05baa0e08421198dad9cce5dcf83bdc811bf58
Senott <senott@yahoo.com>
2016-04-23 20:18:51 -0300

    @DASMacBookAir: 8 adding user accounts.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 // App component - represents the whole app
 class App extends Component {
@@ -25,6 +27,8 @@ class App extends Component {
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
     // Clear form
@@ -60,9 +64,14 @@ class App extends Component {
             Hide completed tasks
           </label>
 
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
-            <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
-          </form>
+          <AccountsUIWrapper />
+
+          { this.props.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+              <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+            </form> : ''
+          }
+
         </header>
         <ul>
           {this.renderTasks()}
@@ -75,11 +84,13 @@ class App extends Component {
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
   incompleteCount: PropTypes.number.isRequired,
+  currentUser: PropTypes.object,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+    currentUser: Meteor.user(),
   };
 }, App);
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -27,7 +27,9 @@ export default class Task extends Component {
         </button>
         <input type="checkbox" readOnly checked={this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}/>
-        <span className="text">{this.props.task.text}</span>
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
       </li>
     );
   }
